0873ebbc9bf988aca436b7428472734bc8ad6ded
jalolovm <muhammadzalolov@gmail.com>
2020-10-26 16:07:34 +0300

    пока не работающий вариант

--- src/main/java/org/springcourse/HelloController.java
+++ src/main/java/org/springcourse/HelloController.java
@@ -1,8 +1,13 @@
 package org.springcourse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
+    @GetMapping("/hello-world")
+    public String sayHello(){
+        return "hello_wold";
+    }
 }
